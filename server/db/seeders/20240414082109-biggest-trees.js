8bd62f5aa88b56f346bc5b7f3d29145512a31c57
Ali Avada <110764863+3alifaisal@users.noreply.github.com>
2024-04-14 14:22:48 +0200

    done phase 2 and phase 3
    String(240) doesn't disallow a max longer than 240 instead it will just take 240 chars if 241 are stored for example
    validate native functions are pretty concise

---
+++ server/db/seeders/20240414082109-biggest-trees.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   await queryInterface.bulkInsert('Trees',[
+    {tree: 'General Sherman', location:'Sequoia National Park',
+     heightFt: 247.9, groundCircumferenceFt: 102.6 },
+     {
+       tree: 'General Grant', location: 'Kings Canyon National Park',
+       heightFt: 268.1, groundCircumferenceFt: 107.5
+     },
+     {
+       tree: 'President', location: 'Sequoia National Park',
+       heightFt: 240.9, groundCircumferenceFt: 93.0
+     },
+    
+     {
+       tree: 'Lincoln', location: 'Sequoia National Park',
+       heightFt: 255.8, groundCircumferenceFt: 98.3
+     },
+     {
+       tree: 'Stagg', location: 'Private Land',
+       heightFt: 243.0, groundCircumferenceFt: 109.0
+     },
+  ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Trees', null, {})
+  }
+};
